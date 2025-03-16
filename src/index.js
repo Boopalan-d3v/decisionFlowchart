5c8692b40050dc4c7e55847edfe8a7be407e6a69
Boopalan-d3v <boopalans0112@gmail.com>
2025-03-17 00:10:25 +0530

    update path for App component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import "./App.css";
+import App from './Components/App';
+import "./Components/App.css";
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
